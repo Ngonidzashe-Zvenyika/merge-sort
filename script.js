4a01c86f85734c3a58c33f73a5991e1f7b2374c0
Ngonidzashe <123906490+Ngonidzashe-Zvenyika@users.noreply.github.com>
2023-10-09 17:18:56 +0200

    Add recursive case to function

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ function mergeSort(arr) {
   if (arr.length <= 1) {
     return arr;
   } else {
+    const mid = Math.round(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
 
+    let i = 0;
+    let j = 0;
+    let mergeArray = [];
+
+    while (i < left.length && j < right.length) {
+      left[i] < right[j]
+        ? mergeArray.push(left[i++])
+        : mergeArray.push(right[j++]);
+    }
+
+    i < left.length
+      ? (mergeArray = mergeArray.concat(left.slice(i, left.length)))
+      : (mergeArray = mergeArray.concat(right.slice(j, right.length)));
+
+    return mergeArray;
   }
 }
